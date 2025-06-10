4dfd16a348f4236bba4a8a9e0701640f88bf1cc3
PRACTICALS <pritiavhad68@gmail.com>
2025-06-10 17:55:10 +0530

    Codes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 let express=require("express");
 let app=express();
 let bodyparser=require("body-parser");
+let session=require("express-session");
+
+let router=require("../src/routes/regRoutes");
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
 app.set('view engine','ejs');
 module.exports=app;
